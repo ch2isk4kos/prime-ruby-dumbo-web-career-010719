7d12339b5e4f26a6bc71a918ae65ef524816778c
Chris Kakos <ckakos@gmail.com>
2018-12-08 02:43:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(int)
+    x = 2
+    prime = int / x
+    while x < prime
+        if int % x == 0
+            return false
+        else
+            return true
+        x += 1
+    end
+end
